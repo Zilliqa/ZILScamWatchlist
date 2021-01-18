069b09b6c12b7a37549391940b4a04258ca3bb86
TIM <timongll@hotmail.com>
2021-01-18 19:05:04 +0800

    first

--- ScamTokenTest.js
+++ ScamTokenTest.js
@@ -3,15 +3,18 @@ const { doesNotMatch } = require('assert');
 const { Test } = require('mocha');
 const fs = require('fs');
 const ZilTest = require('zilliqa-testing-library').default;
-let deployedAddress = "";
-// Save the deployed address here for later use
-contract = fs.readFileSync("scamtoken.scilla").toString();
+let scamTokenAddress = "";
+// Save the scamToken address here for later use
+contract = fs.readFileSync("../contracts/scamtoken.scilla").toString();
+
 describe('ScamToken', function () {
-  /*(beforeEach(async () => {
-    
-}).timeout(10000);
-*/
   const Test = new ZilTest();
+  let scamToken = "";
+  let acc0 = "";
+  let acc1 = "";
+  let acc2 = "";
+  let acc3 = "";
+  
 
   it('Generate 4 Zilliqa accounts on network', async function () {
     await Test.generateAccounts(4);
@@ -22,203 +25,128 @@ describe('ScamToken', function () {
         "9fc50ba5371785cbc5d01ca72b823c080733eb35002b23bb3d2d4a299edf12c0",
         "47877930ebe920d3c5d973073cc3e8ce005926c5c51cf44d1d78c3bc06099c38"
     ]); */
-
+    acc0 = Test.accounts[0].address;
+    acc1 = Test.accounts[1].address;
+    acc2 = Test.accounts[2].address;
+    acc3 = Test.accounts[3].address;
     assert(Test.accounts.length === 4);
-  }).timeout(10000);
+  });
 
   it('Load contract into Testing Suite and run scilla checker', async function () {
     await Test.loadContract(contract); // Contracts[0]
-
     assert(Test.contracts.length === 1);
-  }).timeout(10000);
+  });
 
   it('Deploy ScamToken contract', async function () {
     const preparedContract = Test.contracts[0];
 
     const [tx, deployed] = await preparedContract.deploy(
-      Test.accounts[0].address,
+      acc0,
       {
-        contract_owner: Test.accounts[0].address,
+        contract_owner: acc0,
         name: "ScamToken",
         symbol: "SToken",
         decimals: "12",
         init_supply: "10000",
-        init_manager: Test.accounts[0].address
+        init_management_contract: acc0
       }
     );
 
     assert(tx.receipt.success === true);
 
     // Save for later use
-    deployedAddress = deployed.address;
-  }).timeout(10000);
+    scamTokenAddress = deployed.address;
+    scamToken = Test.deployedContracts[scamTokenAddress];
+  });
 
   // test ChangeManager
-  it('Manager is account[0]', async function () {
-    const deployed = Test.deployedContracts[deployedAddress];
+  it('Management-contract is acc0', async function () {
+    const state = await scamToken.getState();
+    assert(state.management_contract === acc0.toLowerCase());
+  });
 
-    const state = await deployed.getState();
+  it('Manager can change management-contract', async function () {
+    const callTx = await scamToken.ChangeManagementContract(acc0, { new_management_contract: acc1}); 
+    assert(callTx.receipt.success === true);    
+    const state = await scamToken.getState();
+    assert(state.management_contract === acc1.toLowerCase());
+    const callTx2 = await scamToken.ChangeManagementContract(acc1, { new_management_contract: acc0});
+    const state2 = await scamToken.getState();
+    assert(state2.management_contract === acc0.toLowerCase());
+  });
 
-    assert(state.manager === Test.accounts[0].address.toLowerCase());
-  }).timeout(10000);
-
-  xit('Manager can change manager', async function () {
-    const deployed = Test.deployedContracts[deployedAddress];
-    
-    const callTx = await deployed.ChangeManager(Test.accounts[0].address, { new_manager: Test.accounts[1].address});
-    
-    assert(callTx.receipt.success === true);
-    
-    const state = await deployed.getState();
-
-    assert(state.manager === Test.accounts[1].address.toLowerCase());
-  }).timeout(10000);
-
-  it('Non-manager cant change manager', async function () {
-    const deployed = Test.deployedContracts[deployedAddress];
-    
-    const callTx = await deployed.ChangeManager(Test.accounts[1].address, { new_manager: Test.accounts[1].address});
-    
-    assert(callTx.receipt.success === false);
-    
-    const state = await deployed.getState();
-
-    assert(state.manager === Test.accounts[0].address.toLowerCase());
-  }).timeout(10000);
+  it('Non-manager cant change management-contract', async function () {
+    const callTx = await scamToken.ChangeManagementContract(acc1, { new_management_contract: acc1});   
+    assert(callTx.receipt.success === false);    
+    const state = await scamToken.getState();
+    assert(state.management_contract === acc0.toLowerCase());
+  });
 
   //test Mint
-  xit('Mint 1000 tokens, total supply should be 11000 after', async function () {
-    const deployed = Test.deployedContracts[deployedAddress];
-
-    const recipient = Test.accounts[1].address;
-
-    const callTx = await deployed.Mint(Test.accounts[0].address, { recipient: recipient, amount: "1000" });
-
+  it('Mint 1000 tokens, total supply should be 11000 after', async function () {
+    const callTx = await scamToken.Mint(acc0, { recipient: acc1, amount: "1000" });
     assert(callTx.receipt.success === true);
-
-    const state = await deployed.getState();
-
-    assert(state.balances[recipient.toLowerCase()] === '1000');
+    const state = await scamToken.getState();
+    assert(state.balances[acc1.toLowerCase()] === '1000');
     assert(state.total_supply === '11000');
+  });
 
-  }).timeout(10000);
-
-  xit('Mint 1000 tokens twice to same recipient, total supply should be 12000 after', async function () {
-    const deployed = Test.deployedContracts[deployedAddress];
-
-    const recipient = Test.accounts[1].address;
-
-    const callTx = await deployed.Mint(Test.accounts[0].address, { recipient: recipient, amount: "1000" });
-
+  it('Mint 1000 tokens twice to same recipient, total supply should be 13000 after', async function () {
+    const callTx = await scamToken.Mint(acc0, { recipient: acc1, amount: "1000" });
     assert(callTx.receipt.success === true);
-
-    const callTx2 = await deployed.Mint(Test.accounts[0].address, { recipient: recipient, amount: "1000" });
-
+    const callTx2 = await scamToken.Mint(acc0, { recipient: acc1, amount: "1000" });
     assert(callTx2.receipt.success === true);
+    const state = await scamToken.getState();
+    assert(state.balances[acc1.toLowerCase()] === '3000');
+    assert(state.total_supply === '13000');
+  });
 
-    const state = await deployed.getState();
-
-    assert(state.balances[recipient.toLowerCase()] === '2000');
-    assert(state.total_supply === '12000');
-
-  }).timeout(10000);
-
-  it('Fail if Mint is called by non-manager', async function () {
-    const deployed = Test.deployedContracts[deployedAddress];
-
-    const callTx = await deployed.call(Test.accounts[1].address, 'Mint', { recipient: Test.accounts[0].address, amount: "1000" });
-
+  it('Fail if Mint is called by non-management-contract', async function () {
+    const callTx = await scamToken.call(acc1, 'Mint', { recipient: acc0, amount: "1000" });
     assert(callTx.receipt.success === false);
-  }).timeout(10000);
+  });
 
   //test Burn
-  xit('Burn 1000 tokens, total supply should be 9000 after', async function () {
-    const deployed = Test.deployedContracts[deployedAddress];
-
-    const sender = Test.accounts[0].address;
-    const recipient = Test.accounts[0].address;
-
-    const callTx = await deployed.call(sender, 'Burn', { burn_account: recipient, amount: "1000" });
-
+  it('Burn 1000 tokens, total supply should be 12000 after', async function () {
+    const callTx = await scamToken.call(acc0, 'Burn', { burn_account: acc0, amount: "1000" });
     assert(callTx.receipt.success === true);
+    const state = await scamToken.getState();
+    assert(state.balances[acc0.toLowerCase()] === '9000');
+    assert(state.total_supply === '12000');
+  });
 
-    const state = await deployed.getState();
-
-    assert(state.balances[recipient.toLowerCase()] === '9000');
-    assert(state.total_supply === '9000');
-  }).timeout(10000);
-
-  it('Fail if Burn is called by non-manager', async function () {
-    const deployed = Test.deployedContracts[deployedAddress];
-
-    const sender = Test.accounts[1].address;
-    const recipient = Test.accounts[0].address;
-
-    const callTx = await deployed.call(sender, 'Burn', { burn_account: recipient, amount: "1000" });
-
+  it('Fail if Burn is called by non-management-contract', async function () {
+    const callTx = await scamToken.call(acc1, 'Burn', { burn_account: acc0, amount: "1000" });
     assert(callTx.receipt.success === false);
-  }).timeout(10000);
+  });
 
   it('Fail if burn tokens from tokenless address', async function () {
-    const deployed = Test.deployedContracts[deployedAddress];
-
-    const sender = Test.accounts[0].address;
-    const recipient = Test.accounts[1].address;
-
-    const callTx = await deployed.call(sender, 'Burn', { burn_account: recipient, amount: "1000" });
-
+    const callTx = await scamToken.call(acc0, 'Burn', { burn_account: acc2, amount: "1000" });
     assert(callTx.receipt.success === false);
-  }).timeout(10000);
+  });
 
   it('Fail if burn tokens from address with less tokens than burn amount', async function () {
-    const deployed = Test.deployedContracts[deployedAddress];
-
-    const sender = Test.accounts[0].address;
-    const recipient = Test.accounts[0].address;
-
-    const callTx = await deployed.call(sender, 'Burn', { burn_account: recipient, amount: "11000" });
-
+    const callTx = await scamToken.call(acc0, 'Burn', { burn_account: acc0, amount: "10000" });
     assert(callTx.receipt.success === false);
-  }).timeout(10000);
+  });
 
   //test Transfer
-  xit('Transfer 1000 tokens from one account to another', async function() {
-    const deployed = Test.deployedContracts[deployedAddress];
+  it('Transfer 1000 tokens from one account to another', async function() {
+    const callTx = await scamToken.Transfer(acc0, { from: acc0, to: acc1, amount: "1000" });
+    const state = await scamToken.getState();
+    // Sender should have 8000
+    assert(state.balances[acc0.toLowerCase()] === '8000');
+    // Recipient should have 4000
+    assert(state.balances[acc1.toLowerCase()] === '4000');
+  });
 
-    const sender = Test.accounts[0].address;
-    const recipient = Test.accounts[1].address;
-
-    const callTx = await deployed.Transfer(sender, { to: recipient, amount: "1000" });
-
-    const state = await deployed.getState();
-
-    // Sender should have 9000
-    assert(state.balances[sender.toLowerCase()] === '9000');
-
-    // Recipient should have 1000
-    assert(state.balances[recipient.toLowerCase()] === '1000');
-
-  }).timeout(10000);
-
-  it('Fail transfer if sender not manager', async function() {
-    const deployed = Test.deployedContracts[deployedAddress];
-
-    const sender = Test.accounts[1].address;
-    const recipient = Test.accounts[0].address;
-
-    const callTx = await deployed.Transfer(sender, { to: recipient, amount: "1000" });
-
+  it('Fail transfer if sender not management-contract', async function() {
+    const callTx = await scamToken.Transfer(acc1, { from: acc1, to: acc0, amount: "1000" });
     assert(callTx.receipt.success === false);
-  }).timeout(10000);
+  });
 
   it('Fail transfer if sender balance lower than amount', async function() {
-    const deployed = Test.deployedContracts[deployedAddress];
-
-    const sender = Test.accounts[0].address;
-    const recipient = Test.accounts[1].address;
-
-    const callTx = await deployed.Transfer(sender, { to: recipient, amount: "11000" });
-
+    const callTx = await scamToken.Transfer(acc0, { from: acc0, to: acc1, amount: "10000" });
     assert(callTx.receipt.success === false);
-  }).timeout(10000);
+  });
 });
---
+++ ScamURLTest.js
@@ -0,0 +1,151 @@
+var assert = require('assert');
+var chai = require('chai'),
+    expect = chai.expect;
+const { doesNotMatch } = require('assert');
+const { Test } = require('mocha');
+const fs = require('fs');
+const ZilTest = require('zilliqa-testing-library').default;
+let scamURLAddress = "";
+// Save the scamURL address here for later use
+contract = fs.readFileSync("../contracts/scamurl.scilla").toString();
+
+describe('ScamURL', function () {
+  const Test = new ZilTest();
+  let scamURL = "";
+  let acc0 = "";
+  let acc1 = "";
+  let acc2 = "";
+  let acc3 = "";  
+
+  it('Generate 4 Zilliqa accounts on network', async function () {
+    await Test.generateAccounts(4);
+
+    /* await Test.importAccounts([
+        "1129f98cf4fe4c4c694a336f62c6e19d5bbdd5407ccb693c8479b410cc379f72",
+        "8463bc5b65eb18955c29beee3caf153d9b1ca71ec5eb278bd0aa32e86dfbe427",
+        "9fc50ba5371785cbc5d01ca72b823c080733eb35002b23bb3d2d4a299edf12c0",
+        "47877930ebe920d3c5d973073cc3e8ce005926c5c51cf44d1d78c3bc06099c38"
+    ]); */
+    acc0 = Test.accounts[0].address;
+    acc1 = Test.accounts[1].address;
+    acc2 = Test.accounts[2].address;
+    acc3 = Test.accounts[3].address;
+    assert(Test.accounts.length === 4);
+  });
+
+  it('Load contract into Testing Suite and run scilla checker', async function () {
+    await Test.loadContract(contract); // Contracts[0]
+
+    assert(Test.contracts.length === 1);
+  });
+
+  it('Deploy ScamURL contract', async function () {
+    const preparedContract = Test.contracts[0];
+
+    const [tx, deployed] = await preparedContract.deploy(
+      acc0,
+      {
+        contract_owner: acc0,
+        name: "ScamURL",
+        init_management_contract: acc0
+      }
+    );
+
+    assert(tx.receipt.success === true);
+
+    // Save for later use
+    scamURLAddress = deployed.address;
+    scamURL = Test.deployedContracts[scamURLAddress];
+  });
+
+  // test ChangeManager
+  it('Management-contract is account[0]', async function () {
+    const state = await scamURL.getState();
+    assert(state.management_contract === acc0.toLowerCase());
+  });
+
+  it('Manager can change management-contract', async function () {
+    const callTx = await scamURL.ChangeManagementContract(acc0, { new_management_contract: acc1});
+    assert(callTx.receipt.success === true); 
+    const state = await scamURL.getState();
+    assert(state.management_contract === acc1.toLowerCase());
+    const callTx2 = await scamURL.ChangeManagementContract(acc1, { new_management_contract: acc0});
+    const state2 = await scamURL.getState();
+    assert(state2.management_contract === acc0.toLowerCase());
+  });
+
+  it('Non-manager cant change management-contract', async function () {
+    const callTx = await scamURL.ChangeManagementContract(acc1, { new_management_contract: acc1});
+    assert(callTx.receipt.success === false);
+    const state = await scamURL.getState();
+    assert(state.management_contract === acc0.toLowerCase());
+  });
+
+  //test Mint
+  it('Manager can add url', async function () {
+    const callTx = await scamURL.AddURL(acc0, { url: "google.com/horses", category: "phishing" });
+    assert(callTx.receipt.success === true);
+    const state = await scamURL.getState();
+    assert(state.urls["google.com/horses"] === 'phishing');
+
+  });
+
+  it('Non-Manager cant add url', async function () {
+    const callTx = await scamURL.AddURL(acc1, { url: "google.com/horses", category: "phishing" });
+    assert(callTx.receipt.success === false);
+    
+  });
+
+  it('Manager can remove url', async function () {
+    const callTx = await scamURL.AddURL(acc0, { url: "google.com/horses", category: "phishing" });
+    const state = await scamURL.getState();
+    expect(state.urls).to.have.property("google.com/horses");
+    const callTx2 = await scamURL.RemoveURL(acc0, { url: "google.com/horses"});
+    assert(callTx2.receipt.success === true);
+    const state2 = await scamURL.getState();
+    expect(state2.urls).to.not.have.property("google.com/horses");
+
+  });
+
+  it('Non-Manager cant remove url', async function () {
+    const callTx = await scamURL.AddURL(acc0, { url: "google.com/horses", category: "phishing" });
+    const state = await scamURL.getState();
+    expect(state.urls).to.have.property("google.com/horses");
+    const callTx2 = await scamURL.RemoveURL(acc1, { url: "google.com/horses"});
+    assert(callTx2.receipt.success === false);
+    const state2 = await scamURL.getState();
+    expect(state2.urls).to.have.property("google.com/horses");
+  });
+
+  it('Manager can add domain', async function () {
+    const callTx = await scamURL.AddDomain(acc0, { domain: "google.com", category: "phishing" });
+    assert(callTx.receipt.success === true);
+    const state = await scamURL.getState();
+    assert(state.domains["google.com"] === 'phishing');
+  });
+
+  it('Non-Manager cant add domain', async function () {
+    const callTx = await scamURL.AddDomain(acc1, { domain: "google.com", category: "phishing" });
+    assert(callTx.receipt.success === false);
+  });
+
+  it('Manager can remove domain', async function () {
+    const callTx = await scamURL.AddDomain(acc0, { domain: "google.com", category: "phishing" });
+    const state = await scamURL.getState();
+    expect(state.domains).to.have.property("google.com");
+    const callTx2 = await scamURL.RemoveDomain(acc0, { domain: "google.com"});
+    assert(callTx2.receipt.success === true);
+    const state2 = await scamURL.getState();
+    expect(state2.domains).to.not.have.property("google.com");
+  });
+
+  it('Non-Manager cant remove domain', async function () {
+    const callTx = await scamURL.AddDomain(acc0, { domain: "google.com", category: "phishing" });
+    const state = await scamURL.getState();
+    expect(state.domains).to.have.property("google.com");
+    const callTx2 = await scamURL.RemoveDomain(acc1, { domain: "google.com"});
+    assert(callTx2.receipt.success === false);
+    const state2 = await scamURL.getState();
+    expect(state2.domains).to.have.property("google.com");  
+  });
+});
